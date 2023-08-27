71b1512452ca18bb0e7f1c624bacee64a4748354
Juliane <julianebueno1@gmail.com>
2023-08-27 17:10:19 -0300

    feat: :sparkles: Adicionado arquivos bases com pseucódigo/rascunhos

---
+++ README.md
@@ -0,0 +1,5 @@
+## Projeto individual do módulo 4 - CDA
+
+- Seu projeto precisa possuir uma evidência de entrega no arquivo README.md, ou
+seja, um pequeno parágrafo com uma explicação do que foi feito no projeto e a
+defesa das escolhas que você tomou.
---
+++ app.js
@@ -0,0 +1,21 @@
+// O código deve rodar em um laço que vai receber propriedades até a pessoa digitar a palavra “SAIR”. 
+// Quando isso acontecer, deve ser impressa no terminal a lista das propriedades ordenadas de A-Z uma em cada linha.
+
+
+
+// Enquanto escolha diferente que SAIR, leia os atributos
+
+// ---------------------------- "manual"
+// Para cada atributo
+// Primeira letra igual a PL
+// Se a PL desse atributo vier antes do que a PL do proximo atributo: mantem posição
+// Se não inverte as posiçoes
+// Provavel incluir outras condiçoes e tratar erros
+// ---------------------------- "auto"
+// Deve haver comando para "sorted" que organiza um vetor/lista
+
+//Retorna lista organizada
+
+
+
+
