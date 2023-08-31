5d84a9ee25585a614bbc4851d92578774c90961a
Juliane <julianebueno1@gmail.com>
2023-08-31 20:43:26 -0300

    Organizado comentários e apresentação da lista final

--- src/utils/views.js
+++ src/utils/views.js
@@ -2,26 +2,30 @@
 let firula = ".oOo."
 let tamanhoFirula = 14
 
-export function saudacao(){
-    console.log("")
-    console.log(firula.repeat(tamanhoFirula),"\n")
+export function saudacao(){ // primeira mensagem com a info de como sair
+    console.log("\n",firula.repeat(tamanhoFirula),"\n")
     console.log(".  Organizador de Propriedades CSS\n")
     console.log(".  Para SAIR, digite sair\n")
     console.log(firula.repeat(tamanhoFirula),"\n")
 }
 
-export function encerramento(){
-    console.log("")
-    console.log(".  App encerrado\n")
+export function msgSair(){ // mensagem de saída do loop junto do app
+    console.log("\n.  App encerrado\n")
     console.log(firula.repeat(tamanhoFirula),"\n")
 }
 
-export function listaFinal(lista){
-    console.log(lista)
-}
-
-export function msgInputInvalido(){    
+export function msgInputInvalido(){
     console.log(".  Propriedade inválida, digite novamente\n")
 }
 
+export function msgInputValido(inputRecebido){
+    console.log(`.  ${inputRecebido} adicionado na lista\n`)
+}
 
+export function listaFinal(lista){ // apresentando a lista final
+    console.log(".  Lista final com as propriedades digitadas:\n")
+    for (let i = 0; i < lista.length; i++) {
+        console.log(`.  ${lista[i]}`)
+    }
+    console.log("\n",firula.repeat(tamanhoFirula),"\n")
+}
