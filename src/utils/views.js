8ee8deb4d6df28ca02f2777a74281e9a947dde9e
Juliane <julianebueno1@gmail.com>
2023-08-30 22:03:17 -0300

    adição de mensagem para saudação, fim, infos e erros

---
+++ src/utils/views.js
@@ -0,0 +1,27 @@
+// Firula is a necessity, a commodity, a reason to live. Xb
+let firula = ".oOo."
+let tamanhoFirula = 14
+
+export function saudacao(){
+    console.log("")
+    console.log(firula.repeat(tamanhoFirula),"\n")
+    console.log(".  Organizador de Propriedades CSS\n")
+    console.log(".  Para SAIR, digite sair\n")
+    console.log(firula.repeat(tamanhoFirula),"\n")
+}
+
+export function encerramento(){
+    console.log("")
+    console.log(".  App encerrado\n")
+    console.log(firula.repeat(tamanhoFirula),"\n")
+}
+
+export function listaFinal(lista){
+    console.log(lista)
+}
+
+export function msgInputInvalido(){    
+    console.log(".  Propriedade inválida, digite novamente\n")
+}
+
+
