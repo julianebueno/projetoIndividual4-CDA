5547b79a956b93d33ab52e8fd65196a126dc2a73
Juliane <julianebueno1@gmail.com>
2023-08-31 20:39:17 -0300

    Organizado comentários e declaração de variáveis

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // ==============================================================
-// O código deve rodar em um laço que vai receber propriedades até a pessoa digitar a palavra “SAIR”. 
-// Quando isso acontecer, deve ser impressa no terminal a lista das propriedades ordenadas de A-Z uma em cada linha.
+// O código deve rodar em um laço que vai receber propriedades até a pessoa digitar a palavra “SAIR”. Quando isso acontecer, deve ser impressa no terminal a lista das propriedades ordenadas de A-Z uma em cada linha.
 // ==============================================================
 // Pacotes necessários para a execução do app:
 // prompt-sync
@@ -9,41 +8,24 @@
 import * as view from "./src/utils/views.js"
 import * as processo from "./src/controllers/processos.js"
 
-// =============================================== INICIO
-
-console.clear()
-view.saudacao()
-
-let inputRecebido = ""
-let listaItens = []
-let inputValido = ""
+console.clear() // limpa tela
+view.saudacao() // mostra saudação
 
 while (1){
 
-    inputRecebido = processo.receberInput()
-    if (inputRecebido === "sair") {
-        break
+    let inputRecebido = processo.receberInput()
+    if (inputRecebido === "sair") { // condição para encerrar o app
+        break // sai do loop
     }
     
-    inputValido = processo.validarInput(inputRecebido)
+    let inputValido = processo.validarInput(inputRecebido)
     if (!inputValido) {
-        view.msgInputInvalido()
+        view.msgInputInvalido() // mostra mensagem de propriedade recebida inválida
     } else {
-        processo.adicionarNaLista(inputRecebido)
+        view.msgInputValido(inputRecebido) // mostra mensagem de input válido
+        processo.adicionarNaLista(inputRecebido) // adicionado na lista
     }
-
 }
 
-
-view.listaFinal(listaItens)
-
-
-
-
-
-
-console.log("")
-// console.log("")
-
-
-view.encerramento()
+view.msgSair() // mostra mensagem de saída
+view.listaFinal(processo.lista) // mostra a lista final
