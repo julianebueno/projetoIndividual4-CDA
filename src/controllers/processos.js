ff965e73b5d54200fb211fc0141f110289a56641
Juliane <julianebueno1@gmail.com>
2023-08-31 20:41:06 -0300

    Organizado comentários e a função de adicionar na lista

--- src/controllers/processos.js
+++ src/controllers/processos.js
@@ -1,29 +1,23 @@
-
-import * as modelo from "../models/modelos.js"
-import promptSync from 'prompt-sync';
+import * as modelo from "../models/modelos.js" // lista de propriedades válidas
+import promptSync from 'prompt-sync'; // pacote pra ler inputs no terminal
 const prompt = promptSync();
 
-export function receberInput(){
+export function receberInput(){ // recebendo a propriedade digitada
     let inputRecebido = prompt('   Digite uma propriedade CSS: > ');
     return inputRecebido
 }
 
-export function validarInput(inputRecebido){
+export function validarInput(inputRecebido){ // validação do input
     for (let i = 0; i < modelo.listaItens.length; i++) {
         if (inputRecebido == modelo.listaItens[i]) {
-            return true
+            return true // retorna true se tiver na lista modelo de propriedades
         } 
     }
-    return false
+    return false // retorna false, pq se sair do loop é que não tem na lista modelo
 }
 
-//========================================
+export const lista = []
 export function adicionarNaLista(inputRecebido){
-    console.log(`   ${inputRecebido} adicionado na lista\n`)
+    lista.push(inputRecebido) // adicionando o input já validado na lista
+    lista.sort() // organizando a lista em ordem alfabética
 }
-
-
-
-
-
-
